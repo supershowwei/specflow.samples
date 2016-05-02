514fcb56071e69c65eea9ae6c5328a1737f27afc
supershowwei <supershowwei@gmail.com>
2016-05-02 22:34:20 +0800

    create sample feature

---
+++ SpecFlowSamples/SpecFlowSamples/Steps/SampleSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSamples.Steps
+{
+    [Binding]
+    public class SampleSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
