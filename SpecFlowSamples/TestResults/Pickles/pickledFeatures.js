3da51d304e34de75ca924c1553ebf47f423fe215
supershowwei <supershowwei@gmail.com>
2016-05-02 23:17:41 +0800

    run test by specrun generate pickles report

---
+++ SpecFlowSamples/TestResults/Pickles/pickledFeatures.js
@@ -0,0 +1,107 @@
+jsonPWrapper ({
+  "Features": [
+    {
+      "RelativeFolder": "Sample.feature",
+      "Feature": {
+        "Name": "Sample",
+        "Description": "\tIn order to avoid silly mistakes\r\n\tAs a math idiot\r\n\tI want to be told the sum of two numbers",
+        "FeatureElements": [
+          {
+            "Name": "Add two numbers",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I have entered 50 into the calculator",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "And",
+                "NativeKeyword": "And ",
+                "Name": "I have entered 70 into the calculator",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I press add",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "the result should be 120 on the screen",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [
+              "@mytag"
+            ],
+            "Result": {
+              "WasExecuted": true,
+              "WasSuccessful": true
+            }
+          }
+        ],
+        "Result": {
+          "WasExecuted": true,
+          "WasSuccessful": true
+        },
+        "Tags": []
+      },
+      "Result": {
+        "WasExecuted": true,
+        "WasSuccessful": true
+      }
+    }
+  ],
+  "Summary": {
+    "Tags": [
+      {
+        "Tag": "@mytag",
+        "Total": 1,
+        "Passing": 1,
+        "Failing": 0,
+        "Inconclusive": 0
+      }
+    ],
+    "Folders": [
+      {
+        "Folder": "Sample.feature",
+        "Total": 1,
+        "Passing": 1,
+        "Failing": 0,
+        "Inconclusive": 0
+      }
+    ],
+    "NotTestedFolders": [
+      {
+        "Folder": "Sample.feature",
+        "Total": 0,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 0
+      }
+    ],
+    "Scenarios": {
+      "Total": 1,
+      "Passing": 1,
+      "Failing": 0,
+      "Inconclusive": 0
+    },
+    "Features": {
+      "Total": 1,
+      "Passing": 1,
+      "Failing": 0,
+      "Inconclusive": 0
+    }
+  },
+  "Configuration": {
+    "GeneratedOn": "2 五月 2016 23:16:08"
+  }
+});
